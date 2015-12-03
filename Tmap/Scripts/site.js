3f6043f673716880353ad908202c7a8030530ed4
Abdullah Saleem <a.saleem2993@gmail.com>
2015-12-03 16:58:26 +0500

    Initial

---
+++ Tmap/Scripts/site.js
@@ -0,0 +1,59 @@
+﻿var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 2,
+    center: new google.maps.LatLng(0,0),
+    disableDefaultUI: true,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    draggable: false,
+    scrollwheel: false,
+    panControl: false,
+    maxZoom: 2,
+    minZoom: 2
+});
+
+var infowindow = new google.maps.InfoWindow({
+    disableAutoPan: true
+});
+
+var hub = $.connection.tweetHub;
+var marker;
+var content;
+
+$.extend(hub.client, {
+    
+
+    sendTweet: function (lng,ltd,name,date,txt) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(ltd, lng),
+            map: map
+        });
+
+        content = '<div align="left"><strong>Name: </strong> ' + name + '<br><strong>Date: </strong>' + date + '<br><strong>Tweet: </strong>' + txt + '</div>';
+        google.maps.event.addListener(marker, 'mouseover', (function (marker, content, infowindow) {
+            return function () {
+                infowindow.setContent(content);
+                infowindow.open(map, marker);
+            };
+        })(marker, content, infowindow));
+
+        google.maps.event.addListener(marker, 'mouseout', function () {
+            infowindow.close();
+        });
+
+        attachData(marker, content);
+    }
+});
+function attachData(marker, secretMessage) {
+    var infowindow = new google.maps.InfoWindow({
+        content: secretMessage
+    });
+
+    marker.addListener('click', function () {
+        infowindow.open(marker.get('map'), marker);
+    });
+
+}
+
+$.connection.hub.start();
---
+++ Tmap/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Owin;
+using Tweetinvi;
+using Tweetinvi.Core.Credentials;
+using Tweetinvi.Core.Enum;
+using Tweetinvi.Core.Events.EventArguments;
+using Tweetinvi.Core.Interfaces.Streaminvi;
+
+namespace Tmap
+{
+    public class Startup
+    {
+        private static readonly ISampleStream tStream;
+        static Startup()
+        {
+            Auth.ApplicationCredentials = new TwitterCredentials(ConfigurationManager.AppSettings["consumerKey"],
+                ConfigurationManager.AppSettings["consumerSecret"],
+                ConfigurationManager.AppSettings["accessToken"], ConfigurationManager.AppSettings["accessTokenSecret"]);
+            tStream = Stream.CreateSampleStream();
+            tStream.AddTweetLanguageFilter(Language.English);
+            tStream.TweetReceived += tStream_TweetReceived;
+            tStream.StreamStopped += tStream_StreamStopped;
+     
+            tStream.StallWarnings = true;
+            tStream.StartStreamAsync();
+        }
+
+        private static void tStream_StreamStopped(object sender, StreamExceptionEventArgs e)
+        {
+            tStream.StartStream();
+        }
+
+        private static void tStream_TweetReceived(object sender, TweetReceivedEventArgs e)
+        {
+            var t = e.Tweet;
+            //Debug.WriteIf(t.Coordinates==null,"Cords null\n");
+            //Debug.WriteIf(t.Place==null,"Place null\n");
+            
+            if(t.Coordinates==null) return;
+            GlobalHost.ConnectionManager.GetHubContext<TweetHub>()
+                .Clients.All.sendTweet(t.Coordinates.Longitude, t.Coordinates.Latitude, t.CreatedBy.ScreenName,
+                    t.CreatedAt.ToShortTimeString(), t.Text);
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
